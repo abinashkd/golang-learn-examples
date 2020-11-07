6d35e2e41b81443f70c6d15e79b4debbd9fcce4c
Abinash <abidalai@in.ibm.com>
2020-11-07 19:38:46 +0530

    first commit

---
+++ helloworld/helloworld_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Abinash")
+		want := "Hello, Abinash"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
